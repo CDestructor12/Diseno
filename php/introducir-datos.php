38d2d7c85cf8e0fd60c874c53d5c8d27e70c0fcd
Juanma Canahuate <juan.manuel.canahuare@gmail.com>
2020-05-27 18:57:26 -0400

    UI 2.0 added
    Html desing upgraded

--- php/introducir-datos.php
+++ php/introducir-datos.php
@@ -4,8 +4,23 @@
         <meta charset="UTF-8">
         <title>Insertar Datos</title>
         <link rel="icon" href="../img/ucateci-circulo.png">
+        <link rel="stylesheet" href="../css/stilos-principal.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <body style="background-color: white;"></body>
+
     </head>
     <body>
+        <div class="topnav" id="myTopnav">
+            <a href="../index.html">Home</a>  
+            <a href="./lista-reportes-v1.php">Codigo Version 1.0</a>
+            <a href="./lista-reportes-v2.php">Codigo Version 2.0</a>
+            <a href="./lista-reportes-v3.php">Codigo Version 3.0</a>
+            <a class="active" href="./introducir-datos.php">Introducir datos</a>
+            <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                <i class="fa fa-bars"></i>
+            </a>
+        </div>
+        <br>
         <form action="datos.php" method="post">
             Nombre:
             <input type="text" name="PersonaNombre"><br>
@@ -17,5 +32,16 @@
             <input type="text" name="calif"><br>
             <input type="submit" value="INSERTAR">
         </form>
+        <script>
+            function myFunction() {
+                var x = document.getElementById("myTopnav");
+                if (x.className === "topnav") {
+                    x.className += " responsive";
+                }
+                else {
+                    x.className = "topnav";
+                }
+            }
+        </script>
     </body>
 </html>
